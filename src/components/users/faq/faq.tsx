64da1542becc9a3f53167b737d0c1b8f30ebac71
fiiwebarry <fiiwebarry@yahoo.com>
2025-06-06 15:12:32 +0100

    update

--- src/components/users/faq/faq.tsx
+++ src/components/users/faq/faq.tsx
@@ -13,7 +13,7 @@ const Faq = () => {
     setShowFaq((prevId) => (prevId === id ? null : id));
   };
   return (
-    <section className='pt-24 pb-12 px-4 sm:px-6 lg:px-40 lg:pt-3'>
+    <section className=' pt-2 pb-12 px-4 sm:px-6 lg:px-72 lg:pt-3'>
       <div className='grid justify-items-center '>
         <p className='text-2xl mx-auto md:text-4xl font-bold  bg-clip-text text-gray-900 mb-6'>
           Frequently asked questions
@@ -27,7 +27,7 @@ const Faq = () => {
                 className=' grid shadow-2xl bg-white rounded-2xl  p-4'
               >
                 <div className='grid grid-flow-col justify-between'>
-                  <p className='text-3xl md:text-xl text-transparent bg-clip-text  font-bold bg-gradient-to-r from-blue-600 to-purple-600  mb-2'>
+                  <p className='text-md  md:text-xl text-transparent bg-clip-text  font-bold bg-gradient-to-r from-blue-600 to-purple-600  mb-2'>
                     {question}
                   </p>
                   <span onClick={() => toggleFaq(id)}>
@@ -41,9 +41,11 @@ const Faq = () => {
                 {showFaq === id && (
                   <div>
                     <p>{answer}</p>
-                    <p>{slot1}</p>
-                    <p>{slot2}</p>
-                    <p>{slot3}</p>
+                    <ul className='grid pt-2'>
+                      <li>{slot1}</li>
+                      <li>{slot2}</li>
+                      <li>{slot3}</li>
+                    </ul>
                   </div>
                 )}
               </div>
